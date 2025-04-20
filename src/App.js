e81e011c8c8af0cc175584f7e3716cbf68b8904d
kimsa-0605 <holykimsa05@gmail.com>
2025-04-20 13:02:51 +0700

    create react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './layouts/Header';
+import Home from './pages/Home';
+import Delivery from './pages/Delivery';
+import Catalog from './pages/Catalog';
+import About from './pages/About';
+import Contacts from './pages/Contacts';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <div className='container'>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/catalog" element={<Catalog />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contacts />} />
+        </Routes>
+      </div>
+  </Router>
   );
 }
 
